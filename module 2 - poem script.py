ce5433ebb98637ef099769ed4fd379cfbac93b76
Steven van Veldhoven <42697075+Steven2203@users.noreply.github.com>
2022-05-22 15:44:05 +0200

    Add files via upload

---
+++ module 2 - poem script.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module 2 challenge displaying text
+# Purpose:     Write a program that will display a poem
+# Author:      Steven
+# Created:     22-05-2022
+# Copyright:   (c) Steven 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+##I can do it in one line or multiple lines using the print statement.
+##I use the new line character to break each line.
+
+poem =("There once was a movie star icon\nwho preferred to sleep with the light on.\nThey learned how to code\na device that sure glowed\nand lit up the night using Python!")
+print(str(poem))
+
+##Multiple lines example underneath using string literal.
+
+print("There once was a movie star icon")
+print("who preferred to sleep with the light on")
+print("They learned how to code")
+print("a device that sure glowed")
+print("and lit up the night using Python!")
